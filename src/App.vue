69cf4cf1d537a980188a3020a1876fefaa517f11
zenenperaza <peraza@outlook.com>
2023-05-25 19:23:09 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <section>
+    <h3>Add teacher</h3>
+    <form action="">
+      <div><label for="">Nombre:</label><input type="text"></div>
+      <div><label for="">Apellido</label><input type="text"></div>
+      <div><label for="">DNI</label><input type="text"></div>
+      <div><label for="">Materias</label><input type="text"><button>Add materias</button></div>
+      <div><label for="">Docs </label><input type="checkbox"></div>
+      <button mat-button>Add teacher</button>
+
+    </form>
+
+  </section>
+
+  <section>
+<h3>teacher list</h3>
+  </section>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script lang="ts" setup>
+import { Ref, ref  } from "vue";
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+interface ITeacher{
+  teacherName: string,
+  surname: string,
+  dni: string,
+  subjects: Array<string>,
+  doc: boolean
+
+}
+
+let teacher:Ref<ITeacher> = ref({
+  teacherName: '',
+  surname: '',
+  dni: '',
+  subjects: [],
+  doc: false
+})
 </script>
 
+<!-- ESTRUCTURA EN JS
+<script lang="ts" setup>
+import { ref  } from "vue";
+
+let teacher = ref({
+  teacherName: '',
+  surname: '',
+  dni: '',
+  subjects: [],
+  docs: false
+})
+</script> -->
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
