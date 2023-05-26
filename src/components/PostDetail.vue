0c66a66b7ad53cf88e42d81c5bd7241d33da7187
zenenperaza <peraza@outlook.com>
2023-05-26 18:39:06 -0400

    commit 6

--- src/components/PostDetail.vue
+++ src/components/PostDetail.vue
@@ -1,12 +1,42 @@
 <template>
-<div> 
+<div class="post"> 
+    <h3>{{ props.title }}</h3>
+    <p>{{ props.content }}</p>
+
 </div>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: 'PostDetail',
+    props: {
+        title:{
+            type: String,
+            required: true
+        },
+        content : {
+            type: String,
+            requiered: false,
+            default: "Post sin contenido"
+        }
+    },
+    setup(props){
+        return {props}
+    }
+})
    
 </script>
 
 <style scoped>
+.post{
+    width: 300px;
+    height: 200px;
+    border-radius: 10px;
+    background-color: antiquewhite;
+    margin: 10px;
+    padding: 10px;
 
+
+}
 </style>
--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,46 @@
 <template>
-<div> 
-</div>
+    <div class="container">
+        <PostDetail v-for="elm in info" :key="elm.title" :title="elm.title" :content="elm.content"/>
+    </div>
 </template>
 
 <script lang="ts" setup>
-   
+   import PostDetail from './PostDetail.vue';
+
+   interface Post{
+    title: string,
+    content: string
+   }
+
+   let info:Array<Post> = [
+    {
+        title: "Este es el post 1",
+        content: "texto de ejemplo 1"
+    },
+    {
+        title: "Este es el post 2",
+        content: "texto de ejemplo 2"
+    },
+    {
+        title: "Este es el post 3",
+        content: "texto de ejemplo 3"
+    },
+    {
+        title: "Este es el post 4",
+        content: "texto de ejemplo 4"
+    },
+    {
+        title: "Este es el post 5",
+        content: "texto de ejemplo 5"
+    },
+   ]
 </script>
 
 <style scoped>
-
+.container{
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: space-around;
+}
 </style>
