59be9602ce5d2ac1e1d4dc533dda7cda4be054bd
zenenperaza <peraza@outlook.com>
2023-05-25 21:39:37 -0400

    first commit 3

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,46 @@
 <template>
   <section>
     <h3>Add teacher</h3>
-    <form action="">
-      <div><label for="">Nombre:</label><input type="text"></div>
-      <div><label for="">Apellido</label><input type="text"></div>
-      <div><label for="">DNI</label><input type="text"></div>
-      <div><label for="">Materias</label><input type="text"><button>Add materias</button></div>
-      <div><label for="">Docs </label><input type="checkbox"></div>
-      <button mat-button>Add teacher</button>
 
-    </form>
+      <div><label for="">Nombre:</label><input type="text" v-model="teacher.teacherName"></div>
+      <div><label for="">Apellido</label><input type="text" v-model="teacher.surname"></div>
+      <div><label for="">DNI</label><input type="text" v-model="teacher.dni"></div>
+      <div><label for="">Materias</label><input type="text" v-model="subject">
+        <button v-on:click="handleSubject">Add materias</button>
+      </div>
+      <ul>
+        <li v-for="(elm, index) in teacher.subjects" :key="index">{{  elm }}</li>
+      </ul>
+      <div><label for="">Docs </label><input type="checkbox" v-model="teacher.doc"></div>
+      <button mat-button @click="handAddTeacher()">Add teacher</button>
+
+
 
   </section>
 
   <section>
 <h3>teacher list</h3>
+<table>
+  <tr>
+    <th>Nombre</th>
+    <th>Apellido</th>
+    <th>DNI</th>
+    <th>Materias</th>
+    <th>Doc Entregados</th>
+  </tr>
+  <tr v-for="elm in teachers" :key="elm.dni">
+    <th> {{ elm.teacherName }}</th>
+    <th> {{ elm.surname }}</th>
+    <th> {{ elm.dni }}</th>
+    <th> 
+      <ul>
+        <li v-for="(item, index) in elm.subjects" :key="index">{{ item }} </li>
+      </ul>
+    </th>
+    <th v-if="elm.doc"> <p>Entregada</p> </th>
+    <th v-else>No Entregada</th>
+  </tr>
+</table>
   </section>
 </template>
 
@@ -41,6 +67,25 @@ let teacher:Ref<ITeacher> = ref({
 let teachers:Ref<Array<ITeacher>> = ref([])
 
 let subject:Ref<string> = ref('')
+
+const handleSubject = () => {
+  teacher.value.subjects.push(subject.value)
+  subject.value = ''
+}
+const handAddTeacher = () => {
+  teachers.value.push({
+    teacherName: teacher.value.teacherName,
+    surname: teacher.value.surname,
+    dni: teacher.value.dni,
+    subjects: teacher.value.subjects,
+    doc: teacher.value.doc
+  })
+  teacher.value.teacherName = ""
+  teacher.value.surname = ""
+  teacher.value.dni = ""
+  teacher.value.subjects = []
+  teacher.value.doc = false
+}
 </script>
 
 <!-- ESTRUCTURA EN JS
