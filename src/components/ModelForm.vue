ec8fb6f966aca4874c383ee03d8800d29ca6370f
zenenperaza <peraza@outlook.com>
2023-05-27 12:58:21 -0400

    commit 7

---
+++ src/components/ModelForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1 >{{ inputText }} </h1>
+<form action="">
+    <input type="text" v-model="inputText" /><br>
+    <input type="checkbox" v-model="agree"> Acepto los terminos <br>
+    <select name="" id="">
+        <option value="Spain">Spain</option>
+        <option value="UK">UK</option>
+        <option value="Portugal">Portugal</option>
+    </select>
+</form>
+    </template>
+    
+    <script lang="ts" setup>
+
+        import { ref } from "vue";
+
+        let inputText = ref('')
+        let agree = ref(true)
+        let country = ('UK')       
+        
+
+    </script>
+    
+    <style scop>
+    
+    </style>
